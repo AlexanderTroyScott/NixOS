ffc3b193e9e842dc5742d3fb4c70d98e75977d10
Alex <Alexander.Troy.Scott@gmail.com>
2023-07-04 14:50:55 -0500

    Update hardware-settings.nix

--- Xiaomi Book 16/hardware-settings.nix
+++ Xiaomi Book 16/hardware-settings.nix
@@ -8,6 +8,14 @@
   services.tlp = {
   enable = true;
   settings = {
+    SATA_LINKPWR_ON_AC = "med_power_with_dipm";
+    SATA_LINKPWR_ON_BAT = "min_power";
+    PCIE_ASPM_ON_AC = "default";
+    PCIE_ASPM_ON_BAT = "powersave";
+    WIFI_PWR_ON_AC = "off";
+    WIFI_PWR_ON_BAT = "on";
+    RUNTIME_PM_ON_AC = "on";
+    RUNTIME_PM_ON_BAT = "auto";
     CPU_SCALING_GOVERNOR_ON_AC = "performance";
     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
   };
