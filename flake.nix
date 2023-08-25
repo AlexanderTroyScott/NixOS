b801dfd8674f00d8c91549d3885195173a36884e
AlexanderTroyScott <Alexander.Troy.Scott@gmail.com>
2023-08-25 15:16:24 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
         nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";             # Unstable Nix Packages
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
         home-manager = {
-            url = "github:nix-community/home-manager/release-23.05";
+            url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs-unstable";
         };
         
