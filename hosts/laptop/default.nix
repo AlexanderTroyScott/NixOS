627715da018625bb8bd04ce94c1ddbdfe7857777
AlexanderTroyScott <Alexander.Troy.Scott@gmail.com>
2023-08-25 21:36:25 +0100

    test

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -108,7 +108,7 @@
   #Token needs to have read/write Administration priviledges to create runners.
   services.github-runner.tokenFile = "/home/alex/Documents/runner.token";
   services.github-runner.replace = true;
-  services.github-runner.workDir = "/home/alex/Documents/Github";
+  #services.github-runner.workDir = "/home/alex/Documents/Github";
   
   environment.variables.BROWSER = "${pkgs.vivaldi}/bin/vivaldi"; #set default browser
   
