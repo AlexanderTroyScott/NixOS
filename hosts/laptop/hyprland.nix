279e83dc047efff7cf25ad6e94c6bbaecb24fb32
AlexanderTroyScott <Alexander.Troy.Scott@gmail.com>
2023-10-06 02:34:33 +0300

    remove unused file

--- hosts/laptop/hyprland.nix
+++
@@ -1,6 +0,0 @@
-{ config, lib, pkgs, host, system, ... }:
-{
- wayland.windowManager.hyprland.extraConfig = ''
-   source=/home/alex/Documents/NixOS/hosts/laptop/.config/hypr/hyprland.conf
-  '';
-}
