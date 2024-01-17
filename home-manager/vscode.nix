4e4770b0ba71994c0ddad6d84fc96fc9e200270e
AlexanderTroyScott <Alexander.Troy.Scott@gmail.com>
2024-01-16 19:35:08 -0600

    add vscode to home manager

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -1,10 +1,26 @@
 { config, pkgs, ... }:
 {
-    packages = with pkgs; [
-        vscode
-    ];
+
     programs.vscode = {
       enable = true;
-
+      enableUpdateCheck = true;
+      enableExtensionUpdateCheck = true;
+      extensions = with pkgs.vscode-extensions; [
+        github.copilot
+        github.github-vscode-theme
+        github.vscode-github-actions
+        bbenoist.nix
+      ];
+      userSettings = {
+         "window.titleBarStyle" = "custom";
+         "workbench.colorTheme" = "Darcula Amoled Theme";
+         "editor.fontFamily" = "'M+1Code Nerd Font','Droid Sans Mono', 'monospace', monospace";
+         "github.copilot.enable" = {
+           "*" = true;
+           "plaintext" = false;
+           "markdown" = true;
+           "scminput" = false;
+         };
+      };
     };   
 }
