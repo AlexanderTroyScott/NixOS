6052a5f272f4a4b737e1d3b2bd063872768f757e
AlexanderTroyScott <Alexander.Troy.Scott@gmail.com>
2024-05-07 15:25:14 -0500

    remove swap after extending partition

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -28,9 +28,9 @@
       fsType = "ext4";
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/8c324f9e-7f48-43bf-96d5-b16b5f7a67f4"; }
-    ];
+  #swapDevices =
+  #  [ { device = "/dev/disk/by-uuid/8c324f9e-7f48-43bf-96d5-b16b5f7a67f4"; }
+  #  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
