db9c664e9500226c9df12bc794c4a7d6f42d42ed
AlexanderTroyScott <Alexander.Troy.Scott@gmail.com>
2024-07-12 20:56:39 -0500

    add catppuccin.nix

---
+++ home-manager/theme/catppuccin.nix
@@ -0,0 +1,18 @@
+{
+  #https://nix.catppuccin.com/options/home-manager-options.html
+  catppuccin.enable = true;
+  catppuccin.accent = "teal";
+  catppuccin.flavor = "mocha";
+  
+  catppuccin.pointerCursor.enable = true;
+  catppuccin.pointerCursor.accent = "teal";
+  catppuccin.pointerCursor.flavor = "mocha";
+  gtk.catppuccin.enable = true;
+  gtk.catppuccin.accent = "teal";
+  gtk.catppuccin.flavor = "mocha";
+  gtk.catppuccin.gnomeShellTheme = false;
+  gtk.catppuccin.icon.enable = true;
+  gtk.catppuccin.icon.accent = "teal";
+  gtk.catppuccin.icon.flavor = "mocha";
+  gtk.catppuccin.size = "standard";
+}
